3cbedd30a4a6e99df14d94f60b9edbfd1c58597c
Zdeněk Papučík <zdenek.papucik@gmail.com>
2019-10-31 18:19:17 +0100

    dev

--- tests/bash/db.sh
+++ tests/bash/db.sh
@@ -3,5 +3,4 @@ export ORACLE_SID=XE;
 $ORACLE_HOME/bin/sqlplus -S travis/travis <<SQL
 whenever sqlerror exit 2;
 CREATE TABLE test (sample_id INTEGER NOT NULL PRIMARY KEY, sample_string VARCHAR2(255) NOT NULL);
-CREATE TABLE error (id INTEGER NOT NULL PRIMARY KEY, error(...) VARCHAR2(255) NOT NULL);
 SQL
