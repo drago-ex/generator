b65e09fe7e0cc71104bcf9c7bb62b8a03ea8e931
Zdeněk Papučík <zdenek.papucik@gmail.com>
2020-06-20 09:23:06 +0200

    Added new class

---
+++ src/Drago/Generator/Generator/FormDataGenerator.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * Drago Extension
+ * Package built on Nette Framework
+ */
+
+namespace Drago\Generator;
+
+
+/**
+ * Generating form data.
+ */
+class FormDataGenerator extends Base implements IGenerator
+{
+	public function runGeneration(?string $table)
+	{
+		// TODO: Implement runGeneration() method.
+	}
+
+
+	public function createPhpFile(string $table)
+	{
+		// TODO: Implement createPhpFile() method.
+	}
+}
