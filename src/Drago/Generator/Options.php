5af04abfb2d2eac025e2c3daf67ba8f3b80f5f09
accgit <zdenek.papucik@gmail.com>
2021-09-22 16:30:15 +0200

    dev

--- src/Drago/Generator/Options.php
+++ src/Drago/Generator/Options.php
@@ -34,8 +34,8 @@ class Options
 	/** Custom name for the primary key in the table at the constant. */
 	public null|string $primaryKey = null;
 
-    /** Allow basic column info */
-    public bool $columnInfo = false;
+	/** Allow basic column info */
+	public bool $columnInfo = false;
 
 	/** Allow constant. */
 	public bool $constant = true;
