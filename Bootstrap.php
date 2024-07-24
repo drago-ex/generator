b8735ff0ceb0bfbefe2860d213bb9537b9a796b9
Zdeněk Papučík <zdenek.papucik@gmail.com>
2024-07-24 07:24:55 +0200

    Update Bootstrap.php

--- Bootstrap.php
+++ Bootstrap.php
@@ -1,1 +1,16 @@
+<?php
 
+declare(strict_types=1);
+
+
+class Bootstrap
+{
+	public static function boot(): \Nette\Bootstrap\Configurator
+	{
+		$app = new \Nette\Bootstrap\Configurator();
+		$app->setDebugMode(true);
+		$app->setTempDirectory(__DIR__ . '/');
+		$app->addConfig(__DIR__ . '/config.neon');
+		return $app;
+	}
+}
