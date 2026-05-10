7c716bafd324ba586bfd02c7beeb754c17979dce
Zdeněk Papučík <zdenek.papucik@gmail.com>
2026-05-10 17:57:27 +0200

    Update GeneratorExtension.php

--- src/Drago/Generator/DI/GeneratorExtension.php
+++ src/Drago/Generator/DI/GeneratorExtension.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * Test: Drago\Generator\DI\GeneratorExtension
+ */
+
 declare(strict_types=1);
 
 use Drago\Generator\DI\GeneratorExtension;
@@ -8,84 +12,76 @@ use Nette\DI\Compiler;
 use Nette\DI\Container;
 use Nette\DI\ContainerLoader;
 use Tester\Assert;
-use Tester\FileMock;
 use Tester\TestCase;
 
-require __DIR__ . '/../../vendor/autoload.php';
+$container = require __DIR__ . '/../../bootstrap.php';
 
 
 class TestGeneratorExtension extends TestCase
 {
+	protected Container $container;
+
+
+	public function __construct(Container $container)
+	{
+		$this->container = $container;
+	}
+
+
 	public function createContainer(): Container
 	{
-		$tempDir = __DIR__ . '/tmp';
-
-		if (!is_dir($tempDir)) {
-			mkdir($tempDir, 0777, true);
-		}
-
-		$loader = new ContainerLoader($tempDir, true);
-
+		$loader = new ContainerLoader($this->container->getParameters()['tempDir'], true);
 		$class = $loader->load(function (Compiler $compiler): void {
+			$compiler->loadConfig(Tester\FileMock::create('
+			generator:
+				# generator entity
+				path: entity
+				tableName: tableName
+				primaryKey: primaryKeyName
+				columnInfo: true
+				constant: true
+				constantSize: true
+				constantPrefix: Column
+				references: true
+				suffix: Entity
+				extendsOn: true
+				extends: Drago\Database\Entity
+				final: true
+				namespace: App\Entity
 
-			$compiler->loadConfig(FileMock::create('
-generator:
-	path: entity
-	tableName: tableName
-	primaryKey: primaryKeyName
-	columnInfo: true
-	constant: true
-	constantSize: true
-	constantPrefix: Column
-	references: true
-	suffix: Entity
-	extendsOn: true
-	extends: Drago\Database\Entity
-	final: true
-	namespace: App\Entity
+				# generator data class
+				pathDataClass: data
+				constantDataClass: true
+				constantSizeDataClass: true
+				constantDataPrefix: Form
+				referencesDataClass: true
+				suffixDataClass: Data
+				extendsDataClass: Drago\Utils\ExtraArrayHash
+				finalDataClass: false
+				namespaceDataClass: App\Data
 
-	pathDataClass: data
-	constantDataClass: true
-	constantSizeDataClass: true
-	constantDataPrefix: Form
-	referencesDataClass: true
-	suffixDataClass: Data
-	extendsDataClass: Drago\Utils\ExtraArrayHash
-	finalDataClass: false
-	namespaceDataClass: App\Data
-
-services:
-	dibi.connection:
-		factory: Dibi\Connection([
-			driver: mysqli
-			host: 127.0.0.1
-			username: root
-			password: root
-			database: test
-			lazy: true
-		])
+			services:
+				dibi.connection:
+					factory: Dibi\Connection([
+						driver: mysqli
+						host: 127.0.0.1
+						username: root
+						password: root
+						database: test
+						lazy: true
+					])
 			', 'neon'));
-
-			$compiler->addExtension(
-				'generator',
-				new GeneratorExtension
-			);
+			$compiler->addExtension('generator', new GeneratorExtension);
 		});
-
 		return new $class;
 	}
 
 
 	public function test01(): void
 	{
-		$container = $this->createContainer();
-
-		Assert::type(
-			EntityGenerator::class,
-			$container->getByType(EntityGenerator::class)
-		);
+		Assert::type(EntityGenerator::class, $this->createContainer()
+			->getByType(EntityGenerator::class));
 	}
 }
 
-
-(new TestGeneratorExtension())->run();
+(new TestGeneratorExtension($container))->run();
