fdea748c8ef5c1a13a61ebb376326a09a1056cdc
Zdeněk Papučík <zdenek.papucik@gmail.com>
2026-05-10 18:01:31 +0200

    Update GeneratorExtension.php

--- src/Drago/Generator/DI/GeneratorExtension.php
+++ src/Drago/Generator/DI/GeneratorExtension.php
@@ -1,87 +1,135 @@
 <?php
 
-/**
- * Test: Drago\Generator\DI\GeneratorExtension
- */
-
 declare(strict_types=1);
 
-use Drago\Generator\DI\GeneratorExtension;
+namespace Drago\Generator\DI;
+
+use Doctrine\Inflector\Inflector;
+use Doctrine\Inflector\NoopWordInflector;
+use Drago\Database\Entity;
+use Drago\Generator\Command\DataClassCommand;
+use Drago\Generator\Command\EntityCommand;
+use Drago\Generator\Generator\DataClassGenerator;
 use Drago\Generator\Generator\EntityGenerator;
-use Nette\DI\Compiler;
-use Nette\DI\Container;
-use Nette\DI\ContainerLoader;
-use Tester\Assert;
-use Tester\TestCase;
+use Drago\Generator\Options;
+use Drago\Generator\Repository;
+use Drago\Utils\ExtraArrayHash;
+use Nette\DI\CompilerExtension;
+use Nette\DI\Definitions\ServiceDefinition;
+use Nette\Schema\Expect;
+use Nette\Schema\Processor;
+use Nette\Schema\Schema;
+use Symfony\Component\Console\Command\Command;
 
-$container = require __DIR__ . '/../../bootstrap.php';
 
-
-class TestGeneratorExtension extends TestCase
+final class GeneratorExtension extends CompilerExtension
 {
-	protected Container $container;
+	private bool $consoleMode;
 
 
-	public function __construct(Container $container)
+	public function __construct(bool $consoleMode = false)
 	{
-		$this->container = $container;
+		$this->consoleMode = $consoleMode;
 	}
 
 
-	public function createContainer(): Container
+	public function getConfigSchema(): Schema
 	{
-		$loader = new ContainerLoader($this->container->getParameters()['tempDir'], true);
-		$class = $loader->load(function (Compiler $compiler): void {
-			$compiler->loadConfig(Tester\FileMock::create('
-			generator:
-				# generator entity
-				path: entity
-				tableName: tableName
-				primaryKey: primaryKeyName
-				columnInfo: true
-				constant: true
-				constantSize: true
-				constantPrefix: Column
-				references: true
-				suffix: Entity
-				extendsOn: true
-				extends: Drago\Database\Entity
-				final: true
-				namespace: App\Entity
+		return Expect::structure([
+			'lower' => Expect::bool(false),
+			'path' => Expect::string(''),
+			'tableName' => Expect::string()->nullable(),
+			'primaryKey' => Expect::string()->nullable(),
+			'columnInfo' => Expect::bool(false),
+			'constant' => Expect::bool(true),
+			'constantSize' => Expect::bool(false),
+			'constantPrefix' => Expect::string()->nullable(),
+			'references' => Expect::bool(false),
+			'suffix' => Expect::string('Entity'),
+			'extendsOn' => Expect::bool(true),
+			'extends' => Expect::string(Entity::class),
+			'final' => Expect::bool(false),
+			'namespace' => Expect::string('App\Entity'),
 
-				# generator data class
-				pathDataClass: data
-				constantDataClass: true
-				constantSizeDataClass: true
-				constantDataPrefix: Form
-				referencesDataClass: true
-				suffixDataClass: Data
-				extendsDataClass: Drago\Utils\ExtraArrayHash
-				finalDataClass: false
-				namespaceDataClass: App\Data
-
-			services:
-				dibi.connection:
-					factory: Dibi\Connection([
-						driver: mysqli
-						host: 127.0.0.1
-						username: root
-						password: root
-						database: test
-						lazy: true
-					])
-			', 'neon'));
-			$compiler->addExtension('generator', new GeneratorExtension);
-		});
-		return new $class;
+			// Data class
+			'pathDataClass' => Expect::string(''),
+			'constantDataClass' => Expect::bool(true),
+			'constantDataPrefix' => Expect::string()->nullable(),
+			'constantSizeDataClass' => Expect::bool(true),
+			'referencesDataClass' => Expect::bool(false),
+			'suffixDataClass' => Expect::string('Data'),
+			'extendsDataClass' => Expect::string(ExtraArrayHash::class),
+			'finalDataClass' => Expect::bool(false),
+			'namespaceDataClass' => Expect::string('App\Data'),
+		]);
 	}
 
 
-	public function test01(): void
+	public function loadConfiguration(): void
 	{
-		Assert::type(EntityGenerator::class, $this->createContainer()
-			->getByType(EntityGenerator::class));
+		$builder = $this->getContainerBuilder();
+
+		// Repository
+		$builder->addDefinition($this->prefix('repository'))
+			->setFactory(Repository::class);
+
+		// Word inflector
+		$builder->addDefinition($this->prefix('wordInflector'))
+			->setFactory(NoopWordInflector::class);
+
+		// Inflector
+		$builder->addDefinition($this->prefix('inflector'))
+			->setFactory(Inflector::class, [
+				'@' . $this->prefix('wordInflector'),
+				'@' . $this->prefix('wordInflector'),
+			]);
+
+		// Normalize config
+		$processor = new Processor;
+		$options = $processor->process(
+			Expect::from(new Options),
+			$this->config,
+		);
+
+		// Entity generator
+		$builder->addDefinition($this->prefix('entityGenerator'))
+			->setFactory(EntityGenerator::class, [
+				'@' . $this->prefix('repository'),
+				$options,
+				'@' . $this->prefix('inflector'),
+			]);
+
+		// Data class generator
+		$builder->addDefinition($this->prefix('dataClassGenerator'))
+			->setFactory(DataClassGenerator::class, [
+				'@' . $this->prefix('repository'),
+				$options,
+				'@' . $this->prefix('inflector'),
+			]);
+
+		// CLI commands
+		if ($this->consoleMode) {
+			$builder->addDefinition($this->prefix('entityCommand'))
+				->setFactory(EntityCommand::class);
+
+			$builder->addDefinition($this->prefix('dataClassCommand'))
+				->setFactory(DataClassCommand::class);
+		}
+	}
+
+
+	public function beforeCompile(): void
+	{
+		if (!$this->consoleMode) {
+			return;
+		}
+
+		$builder = $this->getContainerBuilder();
+		$commands = $builder->findByType(Command::class);
+
+		foreach ($commands as $serviceName => $serviceDef) {
+			assert($serviceDef instanceof ServiceDefinition);
+			$serviceDef->addTag('console.command');
+		}
 	}
 }
-
-(new TestGeneratorExtension($container))->run();
